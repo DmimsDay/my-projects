a2672e4d169d1f1e1bbacbf79e415a0688ab247b
Dima <DimsdayZ@gmail.com>
2021-12-07 21:16:19 +0300

    Variables, variable types, constant

---
+++ string.php
@@ -0,0 +1,15 @@
+<?php
+
+// Конкатенация 
+$name = "Вася";
+$secondName = "Пупкин";
+
+
+echo '1. Его зовут ' . $name . ' ' . $secondName;
+echo '<br>';
+
+//Присвоение с конкатенацией 
+$hello = '2. Hello ';
+$hello .= 'world!';
+
+echo $hello;
